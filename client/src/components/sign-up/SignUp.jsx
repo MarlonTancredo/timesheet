8f117badf4d7ad975c077375f8e0964733db407e
unknown <marlon_tancredo@hotmail.com>
2023-03-30 15:11:39 +0100

    using useReducer instead of useState

--- client/src/components/sign-up/SignUp.jsx
+++ client/src/components/sign-up/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useReducer, useEffect, useState } from "react";
 
 import Axios from "axios";
 
@@ -8,14 +8,57 @@ import FormButton from "../form-button/FormButton";
 
 const usersPath = "http://localhost:3001/users";
 
+const initialState = {
+  name: "",
+  surname: "",
+  email: "",
+  confirmEmail: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const reducer = (state, action) => {
+  if (action.type === "name") {
+    return { ...state, name: action.value };
+  }
+  if (action.type === "surname") {
+    return { ...state, surname: action.value };
+  }
+  if (action.type === "email") {
+    return { ...state, email: action.value };
+  }
+  if (action.type === "confirmEmail") {
+    return { ...state, confirmEmail: action.value };
+  }
+  if (action.type === "password") {
+    return { ...state, password: action.value };
+  }
+  if (action.type === "confirmPassword") {
+    return { ...state, confirmPassword: action.value };
+  }
+  if (action.type === "clear-all-fields") {
+    return {
+      ...state,
+      name: "",
+      surname: "",
+      email: "",
+      confirmEmail: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+  if (action.type === "clear-email-fields") {
+    return { ...state, email: "", confirmEmail: "" };
+  }
+  if (action.type === "clear-password-fields") {
+    return { ...state, password: "", confirmPassword: "" };
+  }
+  return { ...state };
+};
+
 const SignUp = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [users, setUsers] = useState([]);
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [confirmEmail, setConfirmEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
 
   const getUsers = async () => {
     try {
@@ -54,16 +97,43 @@ const SignUp = () => {
     getUsers();
   }, []);
 
-  const clearAllfields = () => {
-    setName("");
-    setSurname("");
-    setEmail("");
-    setConfirmEmail("");
-    setPassword("");
-    setConfirmPassword("");
+  const clearAllFields = () => {
+    const action = {
+      type: "clear-all-fields",
+    };
+    dispatch(action);
+  };
+
+  const clearEmailFields = () => {
+    const action = {
+      type: "clear-email-fields",
+    };
+    dispatch(action);
+  };
+
+  const clearPasswordFields = () => {
+    const action = {
+      type: "clear-password-fields",
+    };
+    dispatch(action);
+  };
+
+  const handleOnChange = (e) => {
+    const action = {
+      type: e.target.name,
+      value: e.target.value,
+    };
+    dispatch(action);
   };
 
   const handleAddUser = () => {
+    const name = state.name;
+    const surname = state.surname;
+    const email = state.email;
+    const confirmEmail = state.confirmEmail;
+    const password = state.password;
+    const confirmPassword = state.confirmPassword;
+
     if (
       name === "" ||
       surname === "" ||
@@ -73,36 +143,33 @@ const SignUp = () => {
       confirmPassword === ""
     ) {
       alert("You must to fill all fields!");
-      clearAllfields();
+      clearAllFields();
       return;
     }
 
     if (password !== confirmPassword) {
       alert("Passwords must to be equal!");
-      setPassword("");
-      setConfirmPassword("");
+      clearPasswordFields();
       return;
     }
 
     if (email !== confirmEmail) {
       alert("E-mails must to be equals!");
-      setEmail("");
-      setConfirmEmail("");
+      clearEmailFields();
       return;
     }
 
     for (let i = 0; i < users.length; i++) {
       if (email === users[i].email) {
         alert("This e-mail already exists, please enter a different one!");
-        setEmail("");
-        setConfirmEmail("");
+        clearEmailFields();
         return;
       }
     }
 
     saveUser(name, surname, email, confirmEmail, password, confirmPassword);
 
-    clearAllfields();
+    clearAllFields();
   };
 
   return (
@@ -112,50 +179,56 @@ const SignUp = () => {
         <F.Break />
         <F.FieldsNames>Name</F.FieldsNames>
         <F.Input
-          value={name}
+          value={state.name}
+          name="name"
           maxLength="30"
           type="text"
           autoComplete="given-name"
-          onChange={(e) => setName(e.target.value)}
+          onChange={handleOnChange}
         />
         <F.FieldsNames>Last Name</F.FieldsNames>
         <F.Input
-          value={surname}
+          value={state.surname}
+          name="surname"
           maxLength="30"
           type="text"
           autoComplete="family-name"
-          onChange={(e) => setSurname(e.target.value)}
+          onChange={handleOnChange}
         />
         <F.FieldsNames>E-mail</F.FieldsNames>
         <F.Input
-          value={email}
+          value={state.email}
+          name="email"
           maxLength="50"
           type="email"
           autoComplete="email"
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={handleOnChange}
         />
         <F.FieldsNames>Confirm E-mail</F.FieldsNames>
         <F.Input
-          value={confirmEmail}
+          value={state.confirmEmail}
+          name="confirmEmail"
           maxLength="50"
           type="email"
-          onChange={(e) => setConfirmEmail(e.target.value)}
+          onChange={handleOnChange}
         />
         <F.FieldsNames>Password</F.FieldsNames>
         <F.Input
-          value={password}
+          value={state.password}
+          name="password"
           maxLength="30"
           type="password"
           autoComplete="new-password"
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handleOnChange}
         />
         <F.FieldsNames>Confirm Password</F.FieldsNames>
         <F.Input
-          value={confirmPassword}
+          value={state.confirmPassword}
+          name="confirmPassword"
           maxLength="30"
           type="password"
           autoComplete="new-password"
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          onChange={handleOnChange}
         />
         <FormButton formButtonClick={handleAddUser} name="Sign up" />
       </F.FormWrapper>
