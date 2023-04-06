0fd3c09e5556157aae51fcac4f08073c124a3a41
unknown <marlon_tancredo@hotmail.com>
2023-04-06 16:25:07 +0100

    changing sign in and sign up letter to lowercase

--- client/src/components/nav-bar/Navbar.jsx
+++ client/src/components/nav-bar/Navbar.jsx
@@ -5,13 +5,13 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <S.Wrapper>
-      <h1>nav bar</h1>
+      <h1>Time Sheet</h1>
       <S.Links>
         <S.Link>
-          <NavLink to="/">home</NavLink>
+          <NavLink to="/">Home</NavLink>
         </S.Link>
         <S.Link>
-          <NavLink to="/sign-in">Sign In</NavLink>
+          <NavLink to="/sign-in">Sign in</NavLink>
         </S.Link>
         <S.Link>
           <NavLink to="/sign-up">Sign up</NavLink>
