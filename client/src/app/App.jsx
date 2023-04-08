3c5ce18937eb1a4417cab4a24856e59580072678
unknown <marlon_tancredo@hotmail.com>
2023-04-08 13:50:51 +0100

    adding content section in app component

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -6,16 +6,20 @@ import Navbar from "../components/nav-bar/Navbar";
 import HomePage from "../components/home-page/HomePage";
 import SignIn from "../components/sign-in/SignIn";
 import SignUp from "../components/sign-up/SignUp";
+import NotFoundPage from "../components/not-found-page/NotFoundPage";
 
 const App = () => {
   return (
     <S.AppWrapper>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
+      <S.Content>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </S.Content>
     </S.AppWrapper>
   );
 };
