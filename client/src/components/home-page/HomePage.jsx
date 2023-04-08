117e1af8851b46831d824e34fdf5ccaf8bb81fb1
unknown <marlon_tancredo@hotmail.com>
2023-04-08 13:53:56 +0100

    editing home page component

--- client/src/components/home-page/HomePage.jsx
+++ client/src/components/home-page/HomePage.jsx
@@ -1,8 +1,16 @@
+import * as F from "../styles/forms/styles";
+import * as S from "./styles";
+
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <F.Wrapper>
+      <S.StyledLink>
+        <Link to={"/sign-in"}>Enter your login</Link>
+        <Link to={"/sign-up"}>Still not registerd?</Link>
+      </S.StyledLink>
+    </F.Wrapper>
   );
 };
 
